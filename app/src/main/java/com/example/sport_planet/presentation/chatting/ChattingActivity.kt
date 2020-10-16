4512ddb8fdb968fd5d018ebc9efe020e166eadd3
Haemin-Park <phm9368@sookmyung.ac.kr>
2020-10-17 00:23:08 +0900

    add - 채팅 화면 추가

---
+++ app/src/main/java/com/example/sport_planet/presentation/chatting/ChattingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sport_planet.presentation.chatting
+
+import android.os.Bundle
+import com.example.sport_planet.R
+import com.example.sport_planet.databinding.ActivityChattingBinding
+import com.example.sport_planet.presentation.base.BaseActivity
+
+class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity_chatting) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
