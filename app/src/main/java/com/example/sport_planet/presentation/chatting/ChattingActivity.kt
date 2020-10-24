b91c1abf93509485c44eae3f7710d75af2f669be
Haemin-Park <phm9368@sookmyung.ac.kr>
2020-10-24 15:26:24 +0900

    refactor - ChattingViewModel 생성 코드 수정

--- app/src/main/java/com/example/sport_planet/presentation/chatting/ChattingActivity.kt
+++ app/src/main/java/com/example/sport_planet/presentation/chatting/ChattingActivity.kt
@@ -2,18 +2,23 @@ package com.example.sport_planet.presentation.chatting
 
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sport_planet.R
 import com.example.sport_planet.databinding.ActivityChattingBinding
 import com.example.sport_planet.presentation.base.BaseActivity
 import com.example.sport_planet.presentation.chatting.adapter.ChattingAdapter
 import com.example.sport_planet.presentation.chatting.viewmodel.ChattingViewModel
+import com.example.sport_planet.presentation.home.HomeViewModel
 import kotlinx.android.synthetic.main.activity_chatting.*
 
 class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity_chatting) {
 
     private val chattingAdapter = ChattingAdapter(this)
-    private val chattingViewModel = ChattingViewModel()
+    private val chattingViewModel: ChattingViewModel
+         by lazy {
+             ViewModelProvider(this).get(ChattingViewModel::class.java)
+         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
