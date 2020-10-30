df98d2510e2f872d7f9e5881e4c73345d57b4b46
Haemin-Park <phm9368@sookmyung.ac.kr>
2020-10-30 23:30:28 +0900

    add - Date 변환, Time 포맷 변환 유틸 함수 추가

--- app/src/main/java/com/example/sport_planet/util/Util.kt
+++ app/src/main/java/com/example/sport_planet/util/Util.kt
@@ -2,6 +2,8 @@ package com.example.sport_planet.util
 
 import android.content.Context
 import android.util.TypedValue
+import java.text.SimpleDateFormat
+import java.util.*
 
 object Util {
     fun dpToPx(context: Context, dp: Float): Float {
@@ -10,5 +12,17 @@ object Util {
             dp,
             context?.resources?.displayMetrics
         )
+    }
+
+    fun String.toDate(dateFormat: String = "yyyy-MM-dd'T'HH:mm:ss", timeZone: TimeZone = TimeZone.getTimeZone("UTC")): Date {
+        val parser = SimpleDateFormat(dateFormat, Locale.getDefault())
+        parser.timeZone = timeZone
+        return parser.parse(this)
+    }
+
+    fun Date.formatTo(dateFormat: String = "a h:mm", timeZone: TimeZone = TimeZone.getDefault()): String {
+        val formatter = SimpleDateFormat(dateFormat, Locale.KOREA)
+        formatter.timeZone = timeZone
+        return formatter.format(this)
     }
 }
