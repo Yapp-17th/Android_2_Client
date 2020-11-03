e4c0a109c7e7af8fc57cd0d66296c33b5117036f
Haemin-Park <phm9368@sookmyung.ac.kr>
2020-11-04 00:50:30 +0900

    add - 임시 jwt 추가

--- app/src/main/java/com/example/sport_planet/remote/Api.kt
+++ app/src/main/java/com/example/sport_planet/remote/Api.kt
@@ -1,17 +1,15 @@
 package com.example.sport_planet.remote
 
-import com.beust.klaxon.Json
 import com.beust.klaxon.JsonObject
 import com.example.sport_planet.model.*
 //import com.example.sport_planet.model.LoginResponse
 //import com.example.sport_planet.model.ServerCallBackResponse
 import io.reactivex.Single
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
+const val jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJoYWVtaW4iLCJpYXQiOjE2MDQyMjkzMzYsImV4cCI6MTYzNTc2NTMzNywiYXVkIjoiIiwic3ViIjoiaGFlbWluQGdtYWlsLmNvbSIsInVzZXJJZCI6IjIifQ.cgeVPYmMxc5BoPsoeHgl44p-OrBfe3RwjSD1YekTNxo"
 
 interface Api {
+
     @GET("/v1/exercise")
     fun getExercise(): Single<ExerciseResponse>
 
@@ -21,14 +19,17 @@ interface Api {
     //@POST("/user/sign-in")
     //fun postSignIn(@Body userInfo: LoginResponse): Single<ServerCallBackResponse>
 
+    @Headers("Authorization: Bearer $jwt")
     @POST("/v1/chat/room")
     fun makeChattingRoom(
         @Body param: JsonObject
     ): Single<MakeChattingRoomResponse>
 
+    @Headers("Authorization: Bearer $jwt")
     @GET("/v1/chat/room")
     fun getChattingRoomList(): Single<ChattingRoomListResponse>
 
-    @GET("/v1/chat/message/{chatRoomId}")
+    @Headers("Authorization: Bearer $jwt")
+    @GET("/v1/chat/room/{chatRoomId}/message")
     fun getChattingMessageList(@Path("chatRoomId") chatRoomId: Int): Single<ChattingMessageListResponse>
 }
