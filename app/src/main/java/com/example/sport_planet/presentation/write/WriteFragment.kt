575d89f80dfcc981e8eb7272707501a7679ff514
김동환 <donghwan@gimdonghwan-ui-MacBookPro.local>
2020-11-14 22:12:12 +0900

    Fix WriteFragment viewModel, Add Spinner Listener

--- app/src/main/java/com/example/sport_planet/presentation/write/WriteFragment.kt
+++ app/src/main/java/com/example/sport_planet/presentation/write/WriteFragment.kt
@@ -1,5 +1,7 @@
 package com.example.sport_planet.presentation.write
 
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModelProvider
 import com.example.sport_planet.R
@@ -8,15 +10,35 @@ import com.example.sport_planet.presentation.base.BaseFragment
 
 class WriteFragment private constructor() :
     BaseFragment<FragmentWriteBinding, WriteViewModel>(R.layout.fragment_write) {
-    companion object {
-        fun newInstance() = WriteFragment()
+
+    override val viewModel: WriteViewModel by lazy {
+        ViewModelProvider(this).get(WriteViewModel::class.java)
     }
 
-    override val viewModel: WriteViewModel = ViewModelProvider(this).get(WriteViewModel::class.java)
-
     override fun init() {
-        val countingList = (1..15).toList()
-        val adapter = ArrayAdapter(this, R.layout.item_member_count, countingList.toMutableList())
+        val adapter = ArrayAdapter.createFromResource(
+            context!!,
+            R.array.count_list,
+            R.layout.item_member_count
+        )
 
+        binding.spinner.adapter = adapter
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+        }
+    }
+
+    companion object {
+        fun newInstance() = WriteFragment()
     }
 }
