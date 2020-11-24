42ab6dcc8c11257898b9840bf840f34a1b99560a
Haemin-Park <phm9368@sookmyung.ac.kr>
2020-11-25 01:56:18 +0900

    add - 채팅 메세지들이 같은 날에 보낸 것이 아니라면 채팅 날짜로 구분해서 보여주기

--- app/src/main/java/com/example/sport_planet/presentation/chatting/adapter/ChattingAdapter.kt
+++ app/src/main/java/com/example/sport_planet/presentation/chatting/adapter/ChattingAdapter.kt
@@ -2,6 +2,7 @@ package com.example.sport_planet.presentation.chatting.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
@@ -9,8 +10,10 @@ import com.example.sport_planet.data.response.ChattingMessageResponse
 import com.example.sport_planet.databinding.*
 import com.example.sport_planet.presentation.chatting.ChattingConstant
 import com.example.sport_planet.presentation.chatting.UserInfo
+import com.example.sport_planet.util.Util
+import kotlin.properties.Delegates
 
-class ChattingAdapter() : RecyclerView.Adapter<ChattingAdapter.Holder>()
+class ChattingAdapter : RecyclerView.Adapter<ChattingAdapter.Holder>()
 {
 
     private val BOT_NOTICE_MESSAGE_VIEW = 0
@@ -22,6 +25,10 @@ class ChattingAdapter() : RecyclerView.Adapter<ChattingAdapter.Holder>()
     private val SENT_TALK_MESSAGE_VIEW = 6
 
     private var chattingMessages = ArrayList<ChattingMessageResponse>()
+
+    private lateinit var priorDate: String
+    private lateinit var thisDate: String
+    private var isSameDay by Delegates.notNull<Boolean>()
 
     fun settingChattingMessageList(chattingMessageList: ArrayList<ChattingMessageResponse>){
         chattingMessages = chattingMessageList
@@ -39,14 +46,29 @@ class ChattingAdapter() : RecyclerView.Adapter<ChattingAdapter.Holder>()
         fun bind(chattingMessage: ChattingMessageResponse){
             val messageViewType = itemViewType
 
+            thisDate = Util.formatToDate(chattingMessage.createdAt!!)
+            if(position >= 1) {
+                priorDate = Util.formatToDate(chattingMessages[position - 1].createdAt!!)
+                isSameDay = priorDate == thisDate
+            }
+
             when(messageViewType){
 
                 BOT_NOTICE_MESSAGE_VIEW -> {
-                    (binding as ItemChatBotNoticeMessageBinding).itemChatBotNoticeMessage = chattingMessage
+                    (binding as ItemChatBotNoticeMessageBinding).let {
+                        it.itemChatBotNoticeMessage = chattingMessage
+                        it.tvNoticeMessageContent.text = thisDate
+                    }
                 }
 
                 BOT_MESSAGE_VIEW ->{
                     (binding as ItemChatBotMessageBinding).itemChatBotMessage = chattingMessage
+                    if(!isSameDay) {
+                        binding.tvNoticeMessageContent.visibility = View.VISIBLE
+                        binding.tvNoticeMessageContent.text = thisDate
+                    }
+                    else
+                        binding.tvNoticeMessageContent.visibility = View.GONE
                 }
 
                 NOTICE_MESSAGE_VIEW -> {
@@ -57,18 +79,46 @@ class ChattingAdapter() : RecyclerView.Adapter<ChattingAdapter.Holder>()
                     (binding as ItemReceivedProfileMessageBinding).let {
                         it.itemReceivedProfileMessage = chattingMessage
                     }
+                    if(!isSameDay) {
+                        binding.tvNoticeMessageContent.visibility = View.VISIBLE
+                        binding.tvNoticeMessageContent.text = thisDate
+                    }
+                    else
+                        binding.tvNoticeMessageContent.visibility = View.GONE
                 }
+
                 SENT_PROFILE_MESSAGE_VIEW -> {
                     (binding as ItemSentProfileMessageBinding).let {
                         it.itemSentProfileMessage = chattingMessage
                     }
+                    if(!isSameDay){
+                        binding.tvNoticeMessageContent.visibility = View.VISIBLE
+                        binding.tvNoticeMessageContent.text = thisDate
+                    }
+                    else
+                        binding.tvNoticeMessageContent.visibility = View.GONE
                 }
 
                 RECEIVED_TALK_MESSAGE_VIEW -> {
                     (binding as ItemReceivedTalkMessageBinding).itemReceivedTalkMessage = chattingMessage
+                    if(!isSameDay){
+                        binding.tvNoticeMessageContent.visibility = View.VISIBLE
+                        binding.tvNoticeMessageContent.text = thisDate
+                    }
+                    else
+                        binding.tvNoticeMessageContent.visibility = View.GONE
                 }
+
                 SENT_TALK_MESSAGE_VIEW -> {
-                    (binding as ItemSentTalkMessageBinding).itemSentTalkMessage = chattingMessage
+                    (binding as ItemSentTalkMessageBinding).let {
+                        it.itemSentTalkMessage = chattingMessage
+                        if(!isSameDay){
+                            it.tvNoticeMessageContent.visibility = View.VISIBLE
+                            it.tvNoticeMessageContent.text = thisDate
+                        }
+                        else
+                            it.tvNoticeMessageContent.visibility = View.GONE
+                    }
                 }
 
                 else -> throw IllegalArgumentException("적절하지 않은 MessageViewType")
@@ -109,14 +159,17 @@ class ChattingAdapter() : RecyclerView.Adapter<ChattingAdapter.Holder>()
         val messageViewBinding = when(messageViewType){
 
             BOT_NOTICE_MESSAGE_VIEW -> ItemChatBotNoticeMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
             BOT_MESSAGE_VIEW -> ItemChatBotMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
             NOTICE_MESSAGE_VIEW -> ItemNoticeMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
             RECEIVED_PROFILE_MESSAGE_VIEW -> ItemReceivedProfileMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
             SENT_PROFILE_MESSAGE_VIEW -> ItemSentProfileMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
             RECEIVED_TALK_MESSAGE_VIEW -> ItemReceivedTalkMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
             SENT_TALK_MESSAGE_VIEW -> ItemSentTalkMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
             else -> throw IllegalArgumentException("적절하지 않은 MessageViewType")
