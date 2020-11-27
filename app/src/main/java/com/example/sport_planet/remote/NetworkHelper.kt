53171bb99ab88fc06bcbbecc60e5e636a857277b
Haemin-Park <phm9368@sookmyung.ac.kr>
2020-11-27 22:09:27 +0900

    fix - baseUrl 수정

--- app/src/main/java/com/example/sport_planet/remote/NetworkHelper.kt
+++ app/src/main/java/com/example/sport_planet/remote/NetworkHelper.kt
@@ -8,6 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkHelper {
     private const val token =
+        //"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZXN0ZXIyIiwiaWF0IjoxNjA0NjY3MzA5LCJleHAiOjE2MzYyMDMzMTEsImF1ZCI6IiIsInN1YiI6InRlc3RlcjJAZ21haWwuY29tIiwidXNlcklkIjoiMiJ9.iMoSPe9k5Uj6w8qa4eaOIQqjIzI5Scuts3tBbQ4p79g"
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZXN0ZXIxIiwiaWF0IjoxNjA0NjY3MzA5LCJleHAiOjE2MzYyMDMzMTEsImF1ZCI6IiIsInN1YiI6InRlc3RlcjFAZ21haWwuY29tIiwidXNlcklkIjoiMSJ9.Bmbhc-I1r-L-dW5vUzvB9jRsPPKtcqYXutAyWKqkPrc"
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
@@ -22,7 +23,7 @@ object NetworkHelper {
         }.build()
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://13.124.197.125/api/")
+        .baseUrl("http://52.78.52.254/api/")
         .client(okHttpClient)
         .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
         .addConverterFactory(GsonConverterFactory.create())
