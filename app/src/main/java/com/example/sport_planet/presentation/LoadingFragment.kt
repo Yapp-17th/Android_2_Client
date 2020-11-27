bbdaa2cf16d10cb7cdf7a73bad182db36d17f04c
donghwan <ycdoit@naver.com>
2020-11-27 21:05:53 +0900

    Add Loading

---
+++ app/src/main/java/com/example/sport_planet/presentation/LoadingFragment.kt
@@ -0,0 +1,28 @@
+package com.example.sport_planet.presentation
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.sport_planet.R
+
+class LoadingFragment private constructor(): DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_loading,container,false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    companion object {
+        fun newInstance() = LoadingFragment()
+    }
+}
