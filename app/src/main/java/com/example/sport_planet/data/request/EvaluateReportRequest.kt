f9fd54b0962fe9c52e83d4770a9fa0bea777fce0
Mino <k3000js@naver.com>
2020-11-27 15:38:39 +0900

    ADD - 활동완료 탭 구현 완료

--- app/src/main/java/com/example/sport_planet/data/request/EvaluateReportRequest.kt
+++ app/src/main/java/com/example/sport_planet/data/request/EvaluateReportRequest.kt
@@ -3,5 +3,5 @@ package com.example.sport_planet.data.request
 data class EvaluateReportRequest(
     val userId : Long,
     val reportType : Long,
-    val content : String
+    val content : String? = null
 )
--- app/src/main/java/com/example/sport_planet/presentation/mypage/history/FinishTabFragment.kt
+++ app/src/main/java/com/example/sport_planet/presentation/mypage/history/FinishTabFragment.kt
@@ -1,19 +1,59 @@
 package com.example.sport_planet.presentation.mypage.history
 
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.sport_planet.R
+import com.example.sport_planet.data.model.MyViewHistoryModel
+import com.example.sport_planet.data.request.EvaluateReportRequest
 import com.example.sport_planet.databinding.FragmentFinishTabBinding
 import com.example.sport_planet.presentation.base.BaseFragment
 
-class FinishTabFragment : BaseFragment<FragmentFinishTabBinding,FinishTabViewModel>(R.layout.fragment_finish_tab){
+class FinishTabFragment :
+    BaseFragment<FragmentFinishTabBinding, FinishTabViewModel>(R.layout.fragment_finish_tab) {
+    private val finishTabAdapter: FinishTabAdapter by lazy {
+        FinishTabAdapter({ getApplyList(it) }, { showReportDialog(it) }).apply {
+            setHasStableIds(true)
+        }
+    }
     override val viewModel: FinishTabViewModel by lazy {
         ViewModelProvider(this).get(FinishTabViewModel::class.java)
     }
 
     override fun init() {
+        viewModel.getHistory()
+        observeLiveData()
     }
 
-    companion object{
+    private fun observeLiveData() {
+        viewModel.myViewHistoryList.observe(viewLifecycleOwner, Observer {
+            binding.run {
+                rvHistoryFinish.adapter = finishTabAdapter.apply {
+                    setMyViewHistoryItem(viewModel.myViewHistoryList.value!!)
+                }
+            }
+        })
+        viewModel.applyList.observe(viewLifecycleOwner, Observer {
+            binding.run {
+                finishTabAdapter.setApplyListItem(it)
+            }
+        })
+    }
+
+    private fun getApplyList(myViewHistoryModel: MyViewHistoryModel) {
+        viewModel.getApplyList(myViewHistoryModel.boardInfo.boardId)
+    }
+
+    private fun showReportDialog(userId: Long) {
+        val dialog = ReportDialog.newInstance()
+        dialog.setReportDialogListener(object : ReportDialog.ReportDialogListener {
+            override fun onAccept(index: Long, content: String?) {
+                viewModel.postReport(EvaluateReportRequest(userId, index, content))
+            }
+        })
+        dialog.show(parentFragmentManager, "")
+    }
+
+    companion object {
         fun newInstance() = FinishTabFragment()
     }
 }
