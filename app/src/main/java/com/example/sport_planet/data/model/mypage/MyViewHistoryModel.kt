59bbb9f91a38a8a5a4ebbb9925a04735a6f85542
mino <k3000js@naver.com>
2020-12-01 23:44:52 +0900

    필드 추가 부탁드립니다 #66

--- app/src/main/java/com/example/sport_planet/data/model/mypage/MyViewHistoryModel.kt
+++ app/src/main/java/com/example/sport_planet/data/model/mypage/MyViewHistoryModel.kt
@@ -3,9 +3,8 @@ package com.example.sport_planet.data.model.mypage
 import com.example.sport_planet.data.model.BoardInfo
 
 data class MyViewHistoryModel(
-    val host : Boolean,
+    val isHost : Boolean,
     val userName : String,
     val isContinue : Boolean,
-    val leftTime : String,
     val boardInfo : BoardInfo
 )
