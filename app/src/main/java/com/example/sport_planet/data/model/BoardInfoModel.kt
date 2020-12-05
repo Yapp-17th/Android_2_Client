c553492b8d8164bf016688a7f569e5eee1f7a3eb
mino <k3000js@naver.com>
2020-12-05 19:28:22 +0900

    레거시

--- app/src/main/java/com/example/sport_planet/data/model/BoardInfoModel.kt
+++ app/src/main/java/com/example/sport_planet/data/model/BoardInfoModel.kt
@@ -2,14 +2,14 @@ package com.example.sport_planet.data.model
 
 data class BoardInfo(
     val boardId: Long,
+    val hostId: Long,
+    val hostName: String,
     val title: String,
     val groupStatus: String,
     val exercise: String,
     val city: String,
+    val isBookMark: Boolean,
     val recruitNumber: Int,
     val recruitedNumber: Int,
-    val hostId: Long,
-    val hostName: String,
-    val isBookMark: Boolean,
     val time: String
 )
