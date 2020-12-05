0dd2622646a81141b74cf5655ae8a62bd5d377b6
Haemin-Park <phm9368@sookmyung.ac.kr>
2020-12-05 19:28:08 +0900

    fix - 토큰 수정

--- app/src/main/java/com/example/sport_planet/remote/NetworkHelper.kt
+++ app/src/main/java/com/example/sport_planet/remote/NetworkHelper.kt
@@ -9,8 +9,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkHelper {
     private const val token =
-        //"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJoYWVtaW4iLCJpYXQiOjE2MDY5NjMyNjAsImV4cCI6MTYzODQ5OTI2MiwiYXVkIjoiIiwic3ViIjoiaGFlbWluQGV4YW1wbGUuY29tIiwidXNlcklkIjoiMyJ9.3idHYLa5gygsPMJhsiAj9uqiaLt2i6rDx4_POEy86yY"
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzb3llb24iLCJpYXQiOjE2MDY5NjMxNDgsImV4cCI6MTYzODQ5OTE1MCwiYXVkIjoiIiwic3ViIjoic295ZW9uQGV4YW1wbGUuY29tIiwidXNlcklkIjoiMSJ9.zef85bZet10T8GTIfFgC8pNalFlf3I0Ae48aDXnPatw"
+    //"eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MDc1MDQyNTEsImlhdCI6MTYwNzE1ODY1MSwianRpIjoiZTU2MjNhNzAtNTA0Ni00Y2VkLWI2NzUtOGFjZTU4MTYwMjU3IiwidXNlcklkIjoiMSJ9.oo2qv2e0Kjf-YX15XFrCuu486WuP3oKxhodERxaCoXepfJ6KzO_j2c1jPF0cjTLa56W3etiss8Hht2datFjSyA"
+    "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MDc1MDMzNTgsImlhdCI6MTYwNzE1Nzc1OCwianRpIjoiMjJhMzViYjItOWI1Yi00YzRmLThjZjYtMzU1MjBmNTBmY2EzIiwidXNlcklkIjoiMyJ9.9Ha_yquWdpOGoxshm-qUxv9u--34kR3VajQ9GoS5c3joepp2fUbU0wRMbbIu3FYtBJ5SSLuAI6TzvcLxx98bdw"
+
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.NONE
